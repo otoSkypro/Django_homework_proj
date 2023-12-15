dc9aded4ec8e016dd812e2e46dc92c44edde5a34
Семья <oc83@mail.ru>
2023-12-15 13:44:59 +0300

    /

--- catalog/views.py
+++ catalog/views.py
@@ -9,7 +9,7 @@ def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
-        message = request.POST('message')
+        message = request.POST.get('message')
         print(f'Ваше имя {name}, Контактный телефон {phone}, Введите текст {message}')
     return render(request, 'catalog/contacts.html')
 
